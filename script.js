ec40ab3fc3be47c6256b03f05b20d1087dbc6f76
Umar Farooque <umarfarooque1940@gmail.com>
2025-01-26 16:52:34 +0530

    some suggestion changes

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ function typeWriter() {
   }
 }
 typeWriter();
+
+//close menu on click page
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.addEventListener("click", (event) => {
+    if (
+      !navbarCollapse.contains(event.target) && 
+      !navbarToggler.contains(event.target) && 
+      navbarCollapse.classList.contains("show") 
+    ) {
+      bootstrap.Collapse.getInstance(navbarCollapse).hide(); 
+    }
+  });
+
+  navbarCollapse.querySelectorAll(".nav-link").forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      if (navbarCollapse.classList.contains("show")) {
+        bootstrap.Collapse.getInstance(navbarCollapse).hide(); 
+      }
+    });
+  });
+});
